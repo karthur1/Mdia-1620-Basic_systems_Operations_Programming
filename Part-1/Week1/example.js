73dc1138e89607ca72e1cf0818d954c09c14cbf9
kyle Arthur <karthur5@my.bcit.ca>
2022-09-16 12:17:30 -0700

    more code

--- Part-1/Week1/example.js
+++ Part-1/Week1/example.js
@@ -1,4 +1,52 @@
 "Testing" 
-// Declare a varoiable called car
+// Declare a variable called car
 
 var car;
+
+// camelCase
+// Declare a variable called bugscollected and initialise it to the value 4
+var bugsCollected  = 4;
+
+// PascalCase
+// Variable called Bugs Collected and initialise it to the value 4
+var BugsCollected = 4;
+
+// Snake_Case
+var Bugs_Collected = 4;
+
+//Initialize the variable electronics box to the word cpu
+//Use camelCase
+
+var electronicsBox = "cpu";
+
+//Boolean 
+//Initialise the variable called proceed to the value true
+var proceed = true;
+var i = 0
+
+while(proceed) {
+    i++; //i = 1;
+    proceed = false;
+}
+
+console.log(i);
+
+//datat type - object
+/**
+ * 6-4-1 create a variable called grennySmith and add the
+ * following values (use camelCase):
+ * colour of the apple in green
+ * orgin location is australia
+ * orgin date is 1868
+ * ten sonsole log out the colour from the object
+ */
+
+ var grannySmith = {
+	colour: "green",
+    orginLocation: "austalia",
+    orginDate: 1868
+	
+};
+
+
+console.log(grannySmith);
---
+++ Part-1/Week1/test.js
@@ -0,0 +1,8 @@
+var grannySmith = {
+	colour: "green",
+    orginLocation: "austalia",
+    orginDate: 1868
+	
+};
+
+console.log(grannySmith);
